28f21c4181a4f5fc68d1a5ffdd18114f933c9f4c
paristuzun <paristuzun@gmail.com>
2018-12-11 14:45:54 -0500

    built each method

--- index.js
+++ index.js
@@ -4,18 +4,34 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+     let keys = Object.values(collection)
+     for (let i = 0; i < keys.length; i++) {
+       callback(keys[i])
+     }
+     return collection
+   },
 
+    map: function map(collection, callback) {
+
+ //     
+ //     const newArr = [];
+ //
+ //     for (const element of array) {
+ //         newArr.push(callback(element));
+ //     }
+ //
+ //     return newArr;
+ // }
     },
 
-    map: function() {
 
-    },
+
 
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
